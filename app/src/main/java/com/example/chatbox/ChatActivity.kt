7269ecedf75cca9d05e2700eb58e000ecc026853
Ayush Keshri <keshri.ayush019@gmail.com>
2023-05-25 01:56:50 +0530

    modified the ui

--- app/src/main/java/com/example/chatbox/ChatActivity.kt
+++ app/src/main/java/com/example/chatbox/ChatActivity.kt
@@ -27,7 +27,6 @@ class ChatActivity : AppCompatActivity() {
 
     var receiverRoom: String? = null
     var senderRoom: String? = null
-    var image: String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
